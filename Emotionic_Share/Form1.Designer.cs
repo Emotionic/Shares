7719a8b17b345074a254112d8344c46f6be0ba62
c_hakase <gura3_str89@icloud.com>
2017-08-30 13:21:10 +0900

    Twitterの認証でPINを自動で読み取るようにした

--- Emotionic_Share/Form1.Designer.cs
+++ Emotionic_Share/Form1.Designer.cs
@@ -31,18 +31,25 @@
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.file_open = new System.Windows.Forms.Button();
             this.open_filename = new System.Windows.Forms.TextBox();
-            this.pinBox = new System.Windows.Forms.TextBox();
-            this.get_token = new System.Windows.Forms.Button();
-            this.pin_label = new System.Windows.Forms.Label();
             this.filename_label = new System.Windows.Forms.Label();
             this.share_img = new System.Windows.Forms.Button();
             this.tweet_box = new System.Windows.Forms.TextBox();
             this.tab_tw = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.TweLength = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.twitBrow = new System.Windows.Forms.WebBrowser();
+            this.twit_Pass = new System.Windows.Forms.TextBox();
+            this.twit_ID = new System.Windows.Forms.TextBox();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStatus_tw = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.tw_acs = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this._description = new System.Windows.Forms.Label();
             this.movie_description = new System.Windows.Forms.TextBox();
             this._title = new System.Windows.Forms.Label();
@@ -54,14 +61,11 @@
             this.share_mv = new System.Windows.Forms.Button();
             this.movieFname = new System.Windows.Forms.TextBox();
             this.open_moviefile = new System.Windows.Forms.Button();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.webB_YTup = new System.Windows.Forms.WebBrowser();
             this.tab_tw.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.statusStripYT.SuspendLayout();
-            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog
@@ -70,7 +74,7 @@
             // 
             // file_open
             // 
-            this.file_open.Location = new System.Drawing.Point(263, 74);
+            this.file_open.Location = new System.Drawing.Point(263, 99);
             this.file_open.Name = "file_open";
             this.file_open.Size = new System.Drawing.Size(75, 19);
             this.file_open.TabIndex = 1;
@@ -80,41 +84,15 @@
             // 
             // open_filename
             // 
-            this.open_filename.Location = new System.Drawing.Point(33, 74);
+            this.open_filename.Location = new System.Drawing.Point(33, 99);
             this.open_filename.Name = "open_filename";
             this.open_filename.Size = new System.Drawing.Size(224, 19);
             this.open_filename.TabIndex = 2;
             // 
-            // pinBox
-            // 
-            this.pinBox.Location = new System.Drawing.Point(33, 49);
-            this.pinBox.Name = "pinBox";
-            this.pinBox.Size = new System.Drawing.Size(224, 19);
-            this.pinBox.TabIndex = 3;
-            // 
-            // get_token
-            // 
-            this.get_token.Location = new System.Drawing.Point(263, 49);
-            this.get_token.Name = "get_token";
-            this.get_token.Size = new System.Drawing.Size(75, 19);
-            this.get_token.TabIndex = 4;
-            this.get_token.Text = "トークン取得!";
-            this.get_token.UseVisualStyleBackColor = true;
-            this.get_token.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // pin_label
-            // 
-            this.pin_label.AutoSize = true;
-            this.pin_label.Location = new System.Drawing.Point(6, 52);
-            this.pin_label.Name = "pin_label";
-            this.pin_label.Size = new System.Drawing.Size(23, 12);
-            this.pin_label.TabIndex = 5;
-            this.pin_label.Text = "PIN";
-            // 
             // filename_label
             // 
             this.filename_label.AutoSize = true;
-            this.filename_label.Location = new System.Drawing.Point(3, 77);
+            this.filename_label.Location = new System.Drawing.Point(4, 97);
             this.filename_label.Name = "filename_label";
             this.filename_label.Size = new System.Drawing.Size(28, 12);
             this.filename_label.TabIndex = 6;
@@ -123,9 +101,9 @@
             // share_img
             // 
             this.share_img.Font = new System.Drawing.Font("HGS創英角ﾎﾟｯﾌﾟ体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.share_img.Location = new System.Drawing.Point(5, 155);
+            this.share_img.Location = new System.Drawing.Point(6, 203);
             this.share_img.Name = "share_img";
-            this.share_img.Size = new System.Drawing.Size(332, 126);
+            this.share_img.Size = new System.Drawing.Size(332, 109);
             this.share_img.TabIndex = 8;
             this.share_img.Text = "Let\'s Share Photo !";
             this.share_img.UseVisualStyleBackColor = true;
@@ -133,50 +111,116 @@
             // 
             // tweet_box
             // 
-            this.tweet_box.Location = new System.Drawing.Point(5, 99);
+            this.tweet_box.Location = new System.Drawing.Point(6, 135);
             this.tweet_box.Multiline = true;
             this.tweet_box.Name = "tweet_box";
-            this.tweet_box.Size = new System.Drawing.Size(330, 50);
+            this.tweet_box.Size = new System.Drawing.Size(330, 62);
             this.tweet_box.TabIndex = 9;
+            this.tweet_box.TextChanged += new System.EventHandler(this.text_changed);
             // 
             // tab_tw
             // 
             this.tab_tw.Controls.Add(this.tabPage1);
             this.tab_tw.Controls.Add(this.tabPage2);
-            this.tab_tw.Controls.Add(this.tabPage3);
             this.tab_tw.Location = new System.Drawing.Point(3, 2);
             this.tab_tw.Name = "tab_tw";
             this.tab_tw.SelectedIndex = 0;
-            this.tab_tw.Size = new System.Drawing.Size(352, 366);
+            this.tab_tw.Size = new System.Drawing.Size(353, 366);
             this.tab_tw.TabIndex = 10;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.TweLength);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.twitBrow);
+            this.tabPage1.Controls.Add(this.twit_Pass);
+            this.tabPage1.Controls.Add(this.twit_ID);
             this.tabPage1.Controls.Add(this.statusStrip);
             this.tabPage1.Controls.Add(this.share_img);
             this.tabPage1.Controls.Add(this.tw_acs);
             this.tabPage1.Controls.Add(this.tweet_box);
             this.tabPage1.Controls.Add(this.file_open);
-            this.tabPage1.Controls.Add(this.pin_label);
             this.tabPage1.Controls.Add(this.filename_label);
-            this.tabPage1.Controls.Add(this.pinBox);
-            this.tabPage1.Controls.Add(this.get_token);
             this.tabPage1.Controls.Add(this.open_filename);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(344, 340);
+            this.tabPage1.Size = new System.Drawing.Size(345, 340);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Twitter";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // TweLength
+            // 
+            this.TweLength.AutoSize = true;
+            this.TweLength.Location = new System.Drawing.Point(313, 185);
+            this.TweLength.Name = "TweLength";
+            this.TweLength.Size = new System.Drawing.Size(23, 12);
+            this.TweLength.TabIndex = 18;
+            this.TweLength.Text = "140";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "コメント";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 12);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Pass";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ID";
+            // 
+            // twitBrow
+            // 
+            this.twitBrow.Location = new System.Drawing.Point(347, 3);
+            this.twitBrow.MinimumSize = new System.Drawing.Size(20, 20);
+            this.twitBrow.Name = "twitBrow";
+            this.twitBrow.Size = new System.Drawing.Size(215, 302);
+            this.twitBrow.TabIndex = 14;
+            this.twitBrow.Visible = false;
+            this.twitBrow.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.twitBrow_DocumentCompleted);
+            // 
+            // twit_Pass
+            // 
+            this.twit_Pass.Location = new System.Drawing.Point(33, 31);
+            this.twit_Pass.Name = "twit_Pass";
+            this.twit_Pass.Size = new System.Drawing.Size(224, 19);
+            this.twit_Pass.TabIndex = 13;
+            this.twit_Pass.UseSystemPasswordChar = true;
+            // 
+            // twit_ID
+            // 
+            this.twit_ID.Location = new System.Drawing.Point(33, 6);
+            this.twit_ID.Name = "twit_ID";
+            this.twit_ID.Size = new System.Drawing.Size(224, 19);
+            this.twit_ID.TabIndex = 12;
+            // 
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStatus_tw});
+            this.toolStatus_tw,
+            this.toolStripStatusLabel2});
             this.statusStrip.Location = new System.Drawing.Point(3, 315);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(338, 22);
+            this.statusStrip.Size = new System.Drawing.Size(339, 22);
             this.statusStrip.TabIndex = 11;
             // 
             // toolStatus_tw
@@ -184,11 +228,16 @@
             this.toolStatus_tw.Name = "toolStatus_tw";
             this.toolStatus_tw.Size = new System.Drawing.Size(0, 17);
             // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
             // tw_acs
             // 
-            this.tw_acs.Location = new System.Drawing.Point(6, 6);
+            this.tw_acs.Location = new System.Drawing.Point(6, 53);
             this.tw_acs.Name = "tw_acs";
-            this.tw_acs.Size = new System.Drawing.Size(329, 37);
+            this.tw_acs.Size = new System.Drawing.Size(330, 37);
             this.tw_acs.TabIndex = 10;
             this.tw_acs.Text = "Twitterへ接続";
             this.tw_acs.UseVisualStyleBackColor = true;
@@ -196,6 +245,8 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.comboBox2);
             this.tabPage2.Controls.Add(this._description);
             this.tabPage2.Controls.Add(this.movie_description);
             this.tabPage2.Controls.Add(this._title);
@@ -208,15 +259,36 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(344, 340);
+            this.tabPage2.Size = new System.Drawing.Size(345, 340);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "YouTube";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "公開設定";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "public",
+            "private",
+            "unlisted"});
+            this.comboBox2.Location = new System.Drawing.Point(71, 147);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(224, 20);
+            this.comboBox2.TabIndex = 14;
+            // 
             // _description
             // 
             this._description.AutoSize = true;
-            this._description.Location = new System.Drawing.Point(1, 100);
+            this._description.Location = new System.Drawing.Point(1, 93);
             this._description.Name = "_description";
             this._description.Size = new System.Drawing.Size(41, 12);
             this._description.TabIndex = 8;
@@ -227,7 +299,7 @@
             this.movie_description.Location = new System.Drawing.Point(44, 60);
             this.movie_description.Multiline = true;
             this.movie_description.Name = "movie_description";
-            this.movie_description.Size = new System.Drawing.Size(293, 107);
+            this.movie_description.Size = new System.Drawing.Size(293, 77);
             this.movie_description.TabIndex = 7;
             // 
             // _title
@@ -262,7 +334,7 @@
             this.toolStripStatusLabel1});
             this.statusStripYT.Location = new System.Drawing.Point(3, 315);
             this.statusStripYT.Name = "statusStripYT";
-            this.statusStripYT.Size = new System.Drawing.Size(338, 22);
+            this.statusStripYT.Size = new System.Drawing.Size(339, 22);
             this.statusStripYT.TabIndex = 3;
             this.statusStripYT.Text = "statusStrip1";
             // 
@@ -304,32 +376,12 @@
             this.open_moviefile.UseVisualStyleBackColor = true;
             this.open_moviefile.Click += new System.EventHandler(this.open_moviefile_Click);
             // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.webB_YTup);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(344, 340);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // webB_YTup
-            // 
-            this.webB_YTup.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webB_YTup.Location = new System.Drawing.Point(3, 3);
-            this.webB_YTup.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webB_YTup.Name = "webB_YTup";
-            this.webB_YTup.Size = new System.Drawing.Size(338, 334);
-            this.webB_YTup.TabIndex = 0;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(356, 365);
+            this.ClientSize = new System.Drawing.Size(357, 365);
             this.Controls.Add(this.tab_tw);
             this.Name = "Form1";
             this.Text = "Share";
@@ -343,7 +395,6 @@
             this.tabPage2.PerformLayout();
             this.statusStripYT.ResumeLayout(false);
             this.statusStripYT.PerformLayout();
-            this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -353,9 +404,6 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Button file_open;
         private System.Windows.Forms.TextBox open_filename;
-        private System.Windows.Forms.TextBox pinBox;
-        private System.Windows.Forms.Button get_token;
-        private System.Windows.Forms.Label pin_label;
         private System.Windows.Forms.Label filename_label;
         private System.Windows.Forms.Button share_img;
         private System.Windows.Forms.TextBox tweet_box;
@@ -366,9 +414,6 @@
         private System.Windows.Forms.Label _file;
         private System.Windows.Forms.Label _description;
         private System.Windows.Forms.StatusStrip statusStrip;
-        private System.Windows.Forms.ToolStripStatusLabel toolStatus_tw;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.WebBrowser webB_YTup;
         internal System.Windows.Forms.TabPage tabPage2;
         internal System.Windows.Forms.Button open_moviefile;
         internal System.Windows.Forms.Button share_mv;
@@ -378,6 +423,17 @@
         public System.Windows.Forms.ToolStripStatusLabel toolStatus;
         public System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         public System.Windows.Forms.TextBox movieFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        public System.Windows.Forms.WebBrowser twitBrow;
+        public System.Windows.Forms.TextBox twit_Pass;
+        public System.Windows.Forms.TextBox twit_ID;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        public System.Windows.Forms.ToolStripStatusLabel toolStatus_tw;
+        private System.Windows.Forms.Label TweLength;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Emotionic_Share/Form1.cs
+++ Emotionic_Share/Form1.cs
@@ -5,13 +5,11 @@ using static CoreTweet.OAuth;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Collections;
 using System.Reflection;
 using System.Threading;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Upload;
-using Google.Apis.Util.Store;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using System.Threading.Tasks;
@@ -24,14 +22,18 @@ namespace Emotionic_Share
     public partial class Form1 : Form
     {
 
+        public static Tokens tokens = null;
+        public static string PINCode;
+        public OAuthSession session;
+        public static string twid, twps;
+        private static string APIKEY = "rtiYCpTdotGuBlIUXycL4Qm3k";
+        private static string APISECRET = "Hkmg6lBzUu3NHF37zcfVnvKYRNzjlIyz44S9W0SFNnzhCoh3Ip";
+        private static string pin_URL;
+        //private static string _pinsURL;
+        private int tweet = 140;
+        private bool  i = false;
         
-        private static Tokens tokens = null;
-        private string apikey = "rtiYCpTdotGuBlIUXycL4Qm3k";
-        private string apisecret = "Hkmg6lBzUu3NHF37zcfVnvKYRNzjlIyz44S9W0SFNnzhCoh3Ip";
-        private string pincode;
-        private OAuthSession session;
-        public Form1 fm;
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -42,7 +44,9 @@ namespace Emotionic_Share
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            webB_YTup.Navigate("https://www.youtube.com/upload");
+                
+            
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -57,10 +61,7 @@ namespace Emotionic_Share
 
         private void button2_Click(object sender, EventArgs e)
         {
-            pincode = pinBox.Text;
-            //トークン取得
-            tokens = OAuth.GetTokens(session, pincode);
-            if (tokens != null) toolStatus_tw.Text = "Successful acquisition of token!";
+            
         }
 
 
@@ -80,20 +81,25 @@ namespace Emotionic_Share
             img = tokens.Media.Upload(media: new FileInfo(open_filename.Text));
             //ついーと
             Status _status = await tokens.Statuses.UpdateAsync(status => tweet_box.Text + " _Tweet_Success!", media_ids => img);
+            toolStatus_tw.Text = "Tweet Success";
         }
 
         private void tw_acs_Click(object sender, EventArgs e)
         {
-            //PINコード取得
-            session = OAuth.Authorize(apikey, apisecret);
-            Process.Start(session.AuthorizeUri.AbsoluteUri);
+
+            twid = twit_ID.Text;
+            twps = twit_Pass.Text;
+            session = Authorize(APIKEY, APISECRET);
+            pin_URL = session.AuthorizeUri.AbsoluteUri;
+            twitBrow.Navigate(pin_URL);
+
         }
 
 
 
 
-        private static string filename;
-        private static string filePath;
+        private static string filename, filePath, _mtitle, _mdiscre; 
+        
         //以下つべ(未完成
         private void open_moviefile_Click(object sender, EventArgs e)
         {
@@ -106,9 +112,59 @@ namespace Emotionic_Share
                 
             }
         }
+        
+        public void twitBrow_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+            if (i == false)
+            {
+
+                //テキストボックスにIDを設定                
+                HtmlElement id_forms = twitBrow.Document.GetElementById("username_or_email");
+                id_forms.SetAttribute("value", twid);
+                //同じくパスを設定
+                HtmlElement pass_forms = twitBrow.Document.GetElementById("password");
+                pass_forms.SetAttribute("value", twps);
+
+
+                //フォームのサブミット
+                HtmlElement sub_forms = twitBrow.Document.GetElementById("oauth_form");
+                sub_forms.InvokeMember("submit");
+
+
+                i = true;
+            }
+            else if (i == true)
+            {
+
+                //PINの取得
+                HtmlElement _pin = twitBrow.Document.GetElementById("oauth_pin");
+                System.Text.RegularExpressions.MatchCollection mc = System.Text.RegularExpressions.Regex.Matches(_pin.InnerHtml.ToString(), @"<code>(.*?)</code>");
+                foreach (System.Text.RegularExpressions.Match m in mc)
+                {
+                    PINCode = m.Groups[1].Value;
+                }
+                i = false;
+                
+
+                tokens = OAuth.GetTokens(session, PINCode);
+                if (tokens != null) toolStatus_tw.Text = "Successful acquisition of token!";
+            }
+            
+        }
+
+        private void text_changed(object sender, EventArgs e)
+        {
+            tweet -= 1;
+            TweLength.Text = (tweet).ToString();
+            if (tweet < 0) TweLength.ForeColor = System.Drawing.Color.Red; 
+        }
 
         private void share_mv_Click(object sender, EventArgs e)
         {
+            _mtitle = movie_title.Text;
+            _mdiscre = movie_description.Text;
+
            try
             {
 
@@ -125,6 +181,8 @@ namespace Emotionic_Share
             }
 
         }
+
+        
 
         private async Task Run()
         {
@@ -151,13 +209,12 @@ namespace Emotionic_Share
                 
             var video = new Video();
             video.Snippet = new VideoSnippet();
-            video.Snippet.Title = movie_title.Text;
-            video.Snippet.Description = movie_description.Text;
+            video.Snippet.Title = _mtitle;
+            video.Snippet.Description = _mdiscre;
             video.Snippet.Tags = new string[] { "tag1", "tag2" };
             video.Snippet.CategoryId = "22";
             video.Status = new VideoStatus();
-            video.Status.PrivacyStatus = "private";
-            //filePath = movieFname.Text;
+            video.Status.PrivacyStatus = comboBox2.Text;
             filePath = filename;
             using (var fileStream = new FileStream(filePath, FileMode.Open))
             {
@@ -189,8 +246,10 @@ namespace Emotionic_Share
 
         void videosInsertRequest_ResponseReceived(Video video)
         {
+            
             toolStatus.Text = "Video id " + video.Id + "was successfully uploaded.";
             Application.Exit();
+
         }
         
 
